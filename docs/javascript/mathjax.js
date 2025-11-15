4aa95ad7ab1bbd7e08f8736faed5d85600ff1dbe
liblaf <30631553+liblaf@users.noreply.github.com>
2025-11-15 22:32:37 +0800

    feat(docs): Enable MathJax physics package and clean up CSS
    Enable the MathJax physics package to support advanced mathematical notation in documentation. This enhances the ability to render complex physics-related equations and symbols.
    
    Additionally, this commit deprecates and removes the `api-autonav.css` file and its inclusion from `mkdocs.yaml.jinja`. This cleanup is performed because the `mkdocs-api-autonav` plugin is no longer actively used or supported in this template.
    
    Minor stylistic improvements and browser compatibility adjustments are also included in `material.css`.

--- docs/javascript/mathjax.js
+++ docs/javascript/mathjax.js
@@ -6,7 +6,11 @@ window.MathJax = {
 		displayMath: [["\\[", "\\]"]],
 		processEscapes: true,
 		processEnvironments: true,
+		// ref: <https://docs.mathjax.org/en/v3.2/input/tex/extensions/physics.html#physics>
+		packages: { "[+]": ["physics"] },
 	},
+	// ref: <https://docs.mathjax.org/en/v3.2/web/components/combined.html#tex-mml-chtml>
+	loader: { load: ["tex-mml-chtml", "[tex]/physics"] },
 	options: {
 		ignoreHtmlClass: ".*|",
 		processHtmlClass: "arithmatex",
