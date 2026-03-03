07f2a49187dfcfb9c2eb7a622b93d8130cf245e4
liblaf <30631553+liblaf@users.noreply.github.com>
2026-03-03 14:19:27 +0800

    refactor(template): update top-level metadata exports
    Standardize the package's public interface by exporting `__commit_id__` and removing non-dunder version attributes. This ensures consistent use of metadata conventions and provides better visibility into the build's source state.

--- template/src/{{ package_dir }}/__init__.pyi
+++ template/src/{{ package_dir }}/__init__.pyi
@@ -1,3 +1,3 @@
-from ._version import __version__, __version_tuple__, version, version_tuple
+from ._version import __commit_id__, __version__, __version_tuple__
 
-__all__ = ["__version__", "__version_tuple__", "version", "version_tuple"]
+__all__ = ["__commit_id__", "__version__", "__version_tuple__"]
