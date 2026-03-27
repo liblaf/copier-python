5a3e083b9d0897b53355b1e6e1f5a7ddb1836494
liblaf <30631553+liblaf@users.noreply.github.com>
2026-03-27 20:47:44 +0800

    refactor(template): move mise task wiring into Python and TOML
    Replace the shell-based init generator with a Python task that keeps
    module docstrings while rendering the updated lazy-loader stub.
    
    Also move lint task definitions into mise config and update the
    install/upgrade wrappers to use the project root and Python-based sync
    helpers.

--- template/src/{{ package_dir }}/_version.pyi
+++ template/src/{{ package_dir }}/_version.pyi
@@ -1,5 +1,5 @@
 # ref: <https://github.com/ofek/hatch-vcs>
-# ref: <https://github.com/pypa/setuptools-scm/blob/58ed7d5dec0e3bc80190f73543cf4d8a6876ba8e/src/setuptools_scm/_integration/dump_version.py#L16-L51>
+# ref: <https://github.com/pypa/setuptools-scm/blob/0e5d5b0eef6be8ca71c7dbade76cbeb692e24dd0/vcs-versioning/src/vcs_versioning/_dump_version.py#L20-L45>
 __commit_id__: str | None
 __version__: str
 __version_tuple__: tuple[int | str, ...]
