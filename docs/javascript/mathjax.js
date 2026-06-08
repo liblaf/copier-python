cb6262284a287fa08b1a10b30bd39210e77cb2a3
liblaf <30631553+liblaf@users.noreply.github.com>
2026-06-08 17:10:10 +0800

    style: format docs scripts and bench workflow config

--- docs/javascript/mathjax.js
+++ docs/javascript/mathjax.js
@@ -1,25 +1,25 @@
 // ref: <https://squidfunk.github.io/mkdocs-material/reference/math/#mathjax>
 
 window.MathJax = {
-	tex: {
-		inlineMath: [["\\(", "\\)"]],
-		displayMath: [["\\[", "\\]"]],
-		processEscapes: true,
-		processEnvironments: true,
-		// ref: <https://docs.mathjax.org/en/v3.2/input/tex/extensions/physics.html#physics>
-		packages: { "[+]": ["physics"] },
-	},
-	// ref: <https://docs.mathjax.org/en/v3.2/web/components/combined.html#tex-mml-chtml>
-	loader: { load: ["tex-mml-chtml", "[tex]/physics"] },
-	options: {
-		ignoreHtmlClass: ".*|",
-		processHtmlClass: "arithmatex",
-	},
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+    // ref: <https://docs.mathjax.org/en/v3.2/input/tex/extensions/physics.html#physics>
+    packages: { "[+]": ["physics"] },
+  },
+  // ref: <https://docs.mathjax.org/en/v3.2/web/components/combined.html#tex-mml-chtml>
+  loader: { load: ["tex-mml-chtml", "[tex]/physics"] },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex",
+  },
 };
 
 document$.subscribe(() => {
-	MathJax.startup.output.clearCache();
-	MathJax.typesetClear();
-	MathJax.texReset();
-	MathJax.typesetPromise();
+  MathJax.startup.output.clearCache();
+  MathJax.typesetClear();
+  MathJax.texReset();
+  MathJax.typesetPromise();
 });
